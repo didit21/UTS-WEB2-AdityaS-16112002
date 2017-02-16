8a43e39dd61635c6a5020a13b0da615def757f7e
didit21 <aditya.sudiatmo@yahoo.co.id>
2017-02-16 10:14:39 +0700

    version 1.0.0

---
+++ resources/views/temp/header.blade.php
@@ -0,0 +1,10 @@
+<div id="header">
+	<img src=""><center>CRUD ON LARAVEL</center>
+</div>
+<div id="frame-nav">
+	<div id="nav-bar">
+		<li><a href="{{URL::to('/')}}">HOME</a></li>
+		<li><a href="{{URL('krisan')}}">KRITIK & SARAN</a></li>
+		<li><a href="{{URL('login')}}">LOGIN</a></li>
+	</div>
+</div>
